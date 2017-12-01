aa943e38b02180fef4c2bd788d1d9b256e11564a
Noah Halstead <nhalstead00@gmail.com>
2017-11-30 21:05:51 -0500

    Created the Code for the Robot, Added the Drive Control.

---
+++ src/org/usfirst/frc/team5420/robot/Robot.java
@@ -0,0 +1,143 @@
+
+package org.usfirst.frc.team5420.robot;
+
+import edu.wpi.first.wpilibj.CameraServer;
+import edu.wpi.first.wpilibj.GenericHID.Hand;
+import edu.wpi.first.wpilibj.IterativeRobot;
+import edu.wpi.first.wpilibj.Joystick;
+import edu.wpi.first.wpilibj.RobotDrive;
+import edu.wpi.first.wpilibj.Victor;
+import edu.wpi.first.wpilibj.VictorSP;
+import edu.wpi.first.wpilibj.command.Command;
+import edu.wpi.first.wpilibj.command.Scheduler;
+import edu.wpi.first.wpilibj.livewindow.LiveWindow;
+import edu.wpi.first.wpilibj.smartdashboard.SendableChooser;
+import edu.wpi.first.wpilibj.smartdashboard.SmartDashboard;
+
+import org.usfirst.frc.team5420.robot.commands.ExampleCommand;
+import org.usfirst.frc.team5420.robot.subsystems.ExampleSubsystem;
+
+/**
+ * The VM is configured to automatically run this class, and to call the
+ * functions corresponding to each mode, as described in the IterativeRobot
+ * documentation. If you change the name of this class or the package after
+ * creating this project, you must also update the manifest file in the resource
+ * directory.
+ */
+public class Robot extends IterativeRobot {
+
+	public static final ExampleSubsystem exampleSubsystem = new ExampleSubsystem();
+	public static OI oi;
+	protected Command autonomousCommand;
+	protected SendableChooser<Command> chooser = new SendableChooser<>();
+	protected Joystick joystick1, joystick2;
+	
+	protected RobotDrive myDrive;
+	protected VictorSP motorFL, motorBL, motorBR, motorFR;
+	
+	/**
+	 * This function is run when the robot is first started up and should be
+	 * used for any initialization code.
+	 */
+	@SuppressWarnings("deprecation")
+	@Override
+	public void robotInit() {
+		oi = new OI();
+		chooser.addDefault("Default Auto", new ExampleCommand());
+		// chooser.addObject("My Auto", new MyAutoCommand());
+		SmartDashboard.putData("Auto mode", chooser);
+		
+		joystick1 = new Joystick(0);
+		joystick2 = new Joystick(1);
+		
+		motorFL = new VictorSP(3);
+		motorBL = new VictorSP(4);
+		motorBR = new VictorSP(1);
+		motorFR = new VictorSP(2);
+		
+		myDrive = new RobotDrive(motorFL, motorBL, motorBR, motorFR);
+		
+		SmartDashboard.putBoolean("InitComplete", true);
+		SmartDashboard.putInt("COMVersion", 1);
+	}
+
+	/**
+	 * This function is called once each time the robot enters Disabled mode.
+	 * You can use it to reset any subsystem information you want to clear when
+	 * the robot is disabled.
+	 */
+	@Override
+	public void disabledInit() {
+
+	}
+
+	@Override
+	public void disabledPeriodic() {
+		Scheduler.getInstance().run();
+	}
+
+	/**
+	 * This autonomous (along with the chooser code above) shows how to select
+	 * between different autonomous modes using the dashboard. The sendable
+	 * chooser code works with the Java SmartDashboard. If you prefer the
+	 * LabVIEW Dashboard, remove all of the chooser code and uncomment the
+	 * getString code to get the auto name from the text box below the Gyro
+	 *
+	 * You can add additional auto modes by adding additional commands to the
+	 * chooser code above (like the commented example) or additional comparisons
+	 * to the switch structure below with additional strings & commands.
+	 */
+	@Override
+	public void autonomousInit() {
+		autonomousCommand = chooser.getSelected();
+
+		/*
+		 * String autoSelected = SmartDashboard.getString("Auto Selector",
+		 * "Default"); switch(autoSelected) { case "My Auto": autonomousCommand
+		 * = new MyAutoCommand(); break; case "Default Auto": default:
+		 * autonomousCommand = new ExampleCommand(); break; }
+		 */
+
+		// schedule the autonomous command (example)
+		if (autonomousCommand != null)
+			autonomousCommand.start();
+	}
+
+	/**
+	 * This function is called periodically during autonomous
+	 */
+	@Override
+	public void autonomousPeriodic() {
+		Scheduler.getInstance().run();
+	}
+
+	@Override
+	public void teleopInit() {
+		
+		doDrive(joystick1.getY(Hand.kLeft), joystick1.getX(Hand.kLeft), joystick1.getX(Hand.kRight));
+		
+		if (autonomousCommand != null){
+			autonomousCommand.cancel();
+		}
+	}
+
+	/**
+	 * This function is called periodically during operator control
+	 */
+	@Override
+	public void teleopPeriodic() {
+		Scheduler.getInstance().run();
+	}
+
+	/**
+	 * This function is called periodically during test mode
+	 */
+	@Override
+	public void testPeriodic() {
+		LiveWindow.run();
+	}
+	
+	public void doDrive(double frValue, double lrValue, double rValue){
+		myDrive.mecanumDrive_Cartesian(frValue, lrValue, rValue, 0);
+	}
+}
